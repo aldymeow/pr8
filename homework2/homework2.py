da51b221d5b71d21844814362e102daa45519b40
tima <tima.shirinkin@gmail.com>
2024-10-05 13:59:56 +0500

    +homework2

---
+++ homework2/homework2.py
@@ -0,0 +1,7 @@
+while True:
+    print('Введите целые числа: ')
+    a = input()
+    b = input()
+    if isinstance(a, int)==1 and isinstance(b, int)==1:
+        c = a + b
+        print (f"Их сумма = {c}")
